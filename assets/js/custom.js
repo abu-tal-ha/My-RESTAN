302e92da08fd4038325fa8c9dc8347e230a7255e
Abu Talha <mabutalha468@gmail.com>
2024-09-11 13:14:32 +0600

    done

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+
+
+
+
+let menuToggle = document.getElementById('menu-toggle');
+let menuList = document.getElementById('menu-list');
+
+
+menuToggle.addEventListener('click', function() {
+    menuList.classList.toggle('active');
+});
+
+
+
+
